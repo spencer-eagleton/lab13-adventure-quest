04801e6af1e1ffe403dd7a38e83297fe665780fb
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-27 14:10:08 -0700

    appended rendered choices loop to quest page depending on search parameters

--- QUEST/quest.js
+++ QUEST/quest.js
@@ -1,7 +1,30 @@
 import quests from '../quest-data.js';
 import { findByID } from '../utils.js';
-console.log(quests);
+
 
 const params = new URLSearchParams(window.location.search);
+const questData = findByID(quests, params.get('id'));
+console.log(questData);
 
-// const questData = findByID(quests, params.get('id'));
+const title = document.getElementById('quest-title');
+title.textContent = questData.title;
+const img = document.getElementById('quest-image');
+img.src = `../assets/${questData.image}`;
+const description = document.getElementById('quest-description');
+description.textContent = questData.description;
+const questChoices = document.getElementById('quest-choices');
+
+
+for (let choice of questData.choices){
+    const label = document.createElement('label');
+    label.textContent = choice.description;
+    
+
+    const input = document.createElement('input');
+    input.type = 'radio';
+    input.name = 'choices';
+    input.value = choice.id;
+
+    label.appendChild(input);
+    questChoices.append(label);
+}
